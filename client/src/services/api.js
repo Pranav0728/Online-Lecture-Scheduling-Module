a52b53c3aa3e2b3c8e48f649e17d1a126eeae942
Pranav <pranavmolawade123@gmail.com>
2026-06-29 21:17:29 +0530

    new api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://online-lecture-scheduling-module-eight.vercel.app/api';
+const API_URL = 'https://online-lecture-scheduling-module-theta.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
