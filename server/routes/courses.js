25fc58853610dc78e17eca5b067d32027de070e1
Pranav <pranavmolawade123@gmail.com>
2026-06-29 21:29:27 +0530

    new api

--- server/routes/courses.js
+++ server/routes/courses.js
@@ -4,7 +4,7 @@ import upload from '../middleware/multer.js';
 import auth from '../middleware/auth.js';
 const router = express.Router();
 
-router.post('/', auth, upload.single('image'), createCourse);
+router.post('/', auth, createCourse);
 router.get('/', auth, getCourses);
 router.get('/:id', auth, getCourseById);
 router.put('/:id', auth, upload.single('image'), updateCourse);
